d3a400334f81c2a1f3c89ff3b0437b78a322d0bc
shivatgbots <143161324+shivatgbots@users.noreply.github.com>
2023-10-31 16:16:53 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("😊 Join", url = "https://t.me/+Eovs8TI2vNw0ODc9"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
